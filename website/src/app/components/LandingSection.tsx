72bc299ede76badc1aa8c3ec7e5932de343baf9a
shubhamvinnovate <shubham.sutar@vinnovatetechnologies.com>
2025-06-08 16:26:19 +0530

    landing page UI refactor

---
+++ website/src/app/components/LandingSection.tsx
@@ -0,0 +1,33 @@
+'use client';
+import React from 'react';
+import { Button, Col, Container, Row } from 'reactstrap';
+import styles from './landing.module.css';
+
+const LandingSection = () => {
+  return (
+    <Container fluid="sm">
+      <Row xs="1" sm="2">
+        <Col>
+          <div className={styles.landingImg}>
+            {/* <Image src="/model_3.png" alt="model" fill={true} style={{ objectFit: 'cover' }} /> */}
+          </div>
+        </Col>
+        <Col className={`d-flex flex-column justify-content-center align-items-center`}>
+          <p className="text-black text-uppercase fw-medium"># New Summer Collection 2019</p>
+          <h1 className="text-black text-uppercase fw-bolder">Arrivals Sales</h1>
+          <Button
+            color="primary"
+            outline
+            onClick={() => {
+              // setIsAuthenticated(true);
+            }}
+          >
+            Shop Now
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default LandingSection;
--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,19 +1,14 @@
 // import Image from 'next/image';
 import { Col, Container, Row } from 'reactstrap';
-import styles from './page.module.css';
-import ContainerRightSide from './components/ContainerRightSide';
+// import styles from './page.module.css';
+import LandingSection from './components/LandingSection';
 
 export default function Home() {
   return (
-    <Container style={{ backgroundColor: '#F9F9F9' }} fluid>
-      <Row xs="1" sm="2">
+    <Container fluid>
+      <Row xs="1" style={{ backgroundColor: '#F9F9F9' }}>
         <Col>
-          <div className={styles.landingImg}>
-            {/* <Image src="/model_3.png" alt="model" fill={true} style={{ objectFit: 'cover' }} /> */}
-          </div>
-        </Col>
-        <Col className={`d-flex flex-column justify-content-center align-items-center`}>
-          <ContainerRightSide />
+          <LandingSection />
         </Col>
       </Row>
     </Container>
