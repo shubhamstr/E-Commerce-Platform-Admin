49ab1434067302469b88ac0654d4bdbed582442f
shubhamdev <shubhamsutar5799@gmail.com>
2025-08-15 22:38:37 +0530

    axios instance create, installed axios package

---
+++ src/views/Utils/api.js
@@ -0,0 +1,20 @@
+// services/api.ts
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_APP_BASE_NAME, // central base URL
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+// Add token dynamically if needed
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('ecomAdminToken');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
