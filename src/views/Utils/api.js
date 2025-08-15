60f22be38bcd044be94421caf9013a9f284919d4
shubhamdev <shubhamsutar5799@gmail.com>
2025-08-15 22:57:43 +0530

    API service add

--- src/views/Utils/api.js
+++ src/views/Utils/api.js
@@ -7,9 +7,11 @@ const api = axios.create({
     'Content-Type': 'application/json'
   }
 });
+// console.log(import.meta.env.VITE_APP_SERVER_URL);
 
 // Add token dynamically if needed
 api.interceptors.request.use((config) => {
+  // console.log(config);
   const token = localStorage.getItem('ecomAdminToken');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
